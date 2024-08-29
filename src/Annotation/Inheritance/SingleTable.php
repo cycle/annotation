ffbcdcf5f65396a5ec12703c62ba7d42105ccb39
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-08-29 22:01:40 +0300

    Add support integer, float, `Stringable` and backed enumerations as value of `SingleTable` attribute

--- src/Annotation/Inheritance/SingleTable.php
+++ src/Annotation/Inheritance/SingleTable.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Cycle\Annotated\Annotation\Inheritance;
 
+use BackedEnum;
 use Cycle\Annotated\Annotation\Inheritance;
 use Spiral\Attributes\NamedArgumentConstructor;
+use Stringable;
 
 /**
  * @Annotation
@@ -15,9 +17,14 @@ use Spiral\Attributes\NamedArgumentConstructor;
 #[\Attribute(\Attribute::TARGET_CLASS), NamedArgumentConstructor]
 class SingleTable extends Inheritance
 {
+    protected ?string $value;
+
     public function __construct(
-        protected ?string $value = null
+        string|int|float|Stringable|BackedEnum|null $value = null
     ) {
+        $this->value = $value === null
+            ? null
+            : (string) ($value instanceof BackedEnum ? $value->value : $value);
         parent::__construct('single');
     }
 
